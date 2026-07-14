9beda33bdf2a31fb9dc0c7572eb2ce439a9564f4
Rebeca <chake_rebeca@hotmail.com>
2026-07-14 20:49:21 -0300

    registra el servicio y el proveedor concreto

--- backend/src/embeddings/embeddings.module.ts
+++ backend/src/embeddings/embeddings.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { EmbeddingsService } from './embeddings.service';
+import { BedrockTitanEmbeddingProvider } from './bedrock-titan-provider';
+import { EMBEDDING_PROVIDER } from './embedding-provider.interface';
 
 @Module({
   imports: [ConfigModule],
-  providers: [EmbeddingsService],
+  providers: [
+    BedrockTitanEmbeddingProvider,
+    {
+      provide: EMBEDDING_PROVIDER,
+      useExisting: BedrockTitanEmbeddingProvider,
+    },
+    EmbeddingsService,
+  ],
   exports: [EmbeddingsService],
 })
 export class EmbeddingsModule {}
